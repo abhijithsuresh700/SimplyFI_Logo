0e633ce9ae986847fc4bb82bfcc00da75bdc4807
Abhijith Suresh <abhijithsuresh700@gmail.com>
2023-09-22 18:02:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="logoContainer">
+      <div className="square">
+        <div className="text">
+          <h1 className='title'>HTML<span className='and'>&</span>CSS</h1>
+          <h5 className='subTitle'>design and build websites</h5>
+          </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
